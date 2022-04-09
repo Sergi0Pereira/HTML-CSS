9c76f232e53482b6fe50deb61124c943bf9502f1
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-04-09 19:28:24 +0100

    Studying

--- Curso/HTML_Classes/Aulas_Iefp_0792/JavaScript/funcoes.js
+++ Curso/HTML_Classes/Aulas_Iefp_0792/JavaScript/funcoes.js
@@ -1,3 +1,21 @@
-function campeoes () {
-    document.getElementById("campeoes").innerHTML= "Campeoes";
+function campeoes() {
+  document.getElementById("campeoes").innerHTML = "Campeoes";
+}
+
+function imprime() {
+  let jogadores = ["Otávio", "Uribe", "Diogo Costa", "Evanilson"];
+  let msg = "Alguns Jogadores do Porto sao:<br><br> ";
+  for (let j of jogadores) {
+    msg += j + "<br>";
+    alert(j);
+  }
+  document.getElementById("aqui").innerHTML = msg;
+}
+
+function pergunta() {
+  let pergunta1 = "Como se chama?";
+  prompt(pergunta1);
+  document.getElementById("pergunta1").innerHTML = pergunta1;
+
+
 }
