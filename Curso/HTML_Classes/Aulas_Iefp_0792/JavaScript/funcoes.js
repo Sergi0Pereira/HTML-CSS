65ece6cceb9e6301389018352eb0599af72d44e9
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-04-11 19:06:44 +0100

    .

--- Curso/HTML_Classes/Aulas_Iefp_0792/JavaScript/funcoes.js
+++ Curso/HTML_Classes/Aulas_Iefp_0792/JavaScript/funcoes.js
@@ -16,6 +16,34 @@ function pergunta() {
   let pergunta1 = "Como se chama?";
   prompt(pergunta1);
   document.getElementById("pergunta1").innerHTML = pergunta1;
+}
+
+function clube() {
+  let clubes = [" Porto ", " Liverpool ", " Barcelona ", " Juventus "];
+  clubes.sort(); // Ordena os elementos do arreio por ordem alfabetica
+  clubes.reverse(); // Ordena os elementos do arreio de forma reversa
+  document.getElementById("clube").innerHTML = clubes;
+}
+
+function numeros() {
+  let numeros = [12, 13, 45, 6, 5, 9];
+  numeros.sort(function (a, b) {
+  return a - b;}); // Esta funçao é nativa do javaScript e coloca os numeros por ordem
+  numeros.sort(function (a, b) {
+  return a - b;}); // Esta funçao é nativa do javaScript e coloca os numeros por ordem reversa
+  document.getElementById("numeros").innerHTML = numeros;
 
 }
 
+function nomes_numeros() {
+  let numeros = [12, 13, 45, 6, 5, 9];
+  let nomes = ["Joao", "Quintino", "Junior", "Sergio", "Wilson","Ana"]
+  numeros.sort(function (a, b) {return a - b;});
+  nomes.sort();
+  let msg="";
+  for(let i in numeros){
+     msg+=numeros[i]+"- " +nomes[i]+ "<br>";
+  }
+
+  document.getElementById("numeros_nomes").innerHTML = msg;
+}
