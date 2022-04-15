a9f54bdbee744095399f491c7d19d35e954d58e8
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-04-15 01:48:03 +0100

    Página_Pessoal_Homepage

---
+++ Curso/HTML_Classes/Aulas_Iefp_0792/JavaScript/maisciclos.js
@@ -0,0 +1,26 @@
+let soma=0;
+let contador=0;
+
+function media (bt) {
+    if (bt == "para") {
+      document.getElementById("fim").innerHTML = "Fim do Programa";
+      document.getElementById("media").innerHTML = "";
+      soma=0;
+      contador=0;
+    }else {
+      document.getElementById("fim").innerHTML = "";
+      while (bt == "num") {
+        let n = parseInt(document.getElementById("nota").value);
+        soma += n;
+        contador++;
+        break;
+        if (bt == "para") {
+          
+      }
+
+      document.getElementById("nota").value = "";
+      document.getElementById("nota").focus(); //  coloca o apontador na caixa
+      let m = soma / contador;
+      
+    }
+}
